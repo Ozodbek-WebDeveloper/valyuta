57360557063be23f3903904021be14631143cea1
Ozodbek-webDeveloper <ozodbekpanjiyev0444@gmail.com>
2025-06-09 19:27:28 +0500

    update

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,34 @@
   <div class="w-[500px] mx-auto border mt-4 py-2 px-2 rounded-md">
     <h1 class="text-center text-2xl mb-10">valyuta converter</h1>
     <div class="flex justify-between items-center">
+      <!--  -->
       <div class="w-33">
-        <v-select v-model="selectCurrency" label="Select" density="compact" class="h-[40px]"
-          :items="currencies"></v-select>
+        <select class="w-full border px-2" v-model="selectCurrency">
+          <option value="" disabled selected hidden>select</option>
+          <option :value="item" v-for="item in currencies">{{ item }}</option>
+        </select>
       </div>
+      <!--  -->
       <p><i class="fa-solid fa-right-long"></i></p>
+      <!--  -->
       <div class="w-33">
-        <v-select v-model="toCurrency" label="Select" class="h-[40px]" density="compact" :items="currencies"></v-select>
+        <select class="w-full border px-2" v-model="toCurrency">
+          <option value="" disabled selected hidden>select</option>
+          <option :value="item" v-for="item in currencies">{{ item }}</option>
+        </select>
       </div>
+      <!--  -->
     </div>
     <div class=" my-4 w-33 mx-auto">
       <input type="number" v-model="changes" class="outline-none pl-2 w-full" style="border: 1px solid black;">
     </div>
+    <!--  -->
     <div class="flex justify-center gap-3">
-      <v-btn @click="result">result</v-btn>
-      <v-btn @click="reset">reset</v-btn>
+      <button style="background-color: green;" :class="{ 'disabled': activeBtn }"
+        class="btn text-white px-5 py-1 rounded-md" @click="result">result</button>
+      <button style="background-color: yellow;" class="text-black px-5 py-1 rounded-md" @click="reset">reset</button>
     </div>
+    <!--  -->
     <p class="mt-5">{{ answer }}</p>
   </div>
 </template>
@@ -32,9 +44,11 @@ const toCurrency = ref('')
 const currencies = ref(['USD', 'EUR', 'UZS', 'RUB'])
 const answer = ref('0')
 const changes = ref('')
+const activeBtn = ref(true)
 // 
 
 async function result() {
+  if (!activeBtn) return
   if (!selectCurrency.value || !toCurrency.value || !changes.value) return
 
   try {
@@ -42,12 +56,12 @@ async function result() {
     const rate = res.data.conversion_rates[toCurrency.value]
 
     const amount = +changes.value
-    answer.value = `${amount} ${selectCurrency.value} =   ${(rate * amount).toFixed(2)} ${toCurrency.value}`
+    answer.value = `${amount} ${selectCurrency.value} = ${(rate * amount).toFixed(2)} ${toCurrency.value}`
   } catch (error) {
-    console.log(error);    
+    console.log(error);
   }
-
 }
+
 
 function reset() {
   selectCurrency.value = ''
@@ -55,6 +69,11 @@ function reset() {
   changes.value = ''
   answer.value = ''
 }
+
+watch([selectCurrency, toCurrency, changes], ([select, to, change]) => {
+  activeBtn.value = !(select && to && change);
+})
+
 
 watch([selectCurrency, toCurrency,], ([from, to,]) => {
   if (from && to) {
@@ -66,7 +85,7 @@ async function getCurrency(from: string, to: string,) {
   try {
     const res = await axios.get(`${API.value}${from}`)
     const rate = res.data.conversion_rates[to]
-    answer.value = `1 ${from} = ${rate} ${to}`
+    answer.value = `1 ${from} = ${rate.toFixed(2)} ${to}`
   } catch (error) {
     console.log(error)
   }
@@ -75,4 +94,9 @@ async function getCurrency(from: string, to: string,) {
 
 </script>
 <!--  -->
-<style></style>
+<style>
+.btn.disabled {
+  opacity: 0.6;
+  cursor: no-drop;
+}
+</style>
