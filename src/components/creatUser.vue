b31ae28d792c4af084ae6b527c251981bd6bd4f8
Ozodbek-webDeveloper <ozodbekpanjiyev0444@gmail.com>
2025-06-10 18:59:31 +0500

    git init

---
+++ src/components/creatUser.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="create">
+    <form @click.prevent>
+      <input type="text" placeholder="UserName" v-model="UserData.UserName">
+      <input type="text" placeholder="FullName" v-model="UserData.fullName">
+      <input type="text" placeholder="Middle Name" v-model="UserData.MiddleName">
+      <select name="" id="" v-model="UserData.Status">
+        <option :value="true">active</option>
+        <option :value="false">disActive</option>
+      </select>
+      <input type="text" placeholder="Information" v-model="UserData.information">
+      <div class="flex flex-column gap-2">
+        <button class="bg-green px-4" @click="saved">
+          save
+        </button>
+        <button class="bg-yellow">
+          clear
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+const emit = defineEmits(['save'])
+
+const props = defineProps<{ user: CreateUser }>()
+
+interface CreateUser {
+  id?: number
+  UserName: string,
+  fullName: string,
+  MiddleName: string,
+  Status: boolean,
+  information: string
+}
+
+const UserData = ref<CreateUser>({
+  UserName: '',
+  fullName: '',
+  MiddleName: '',
+  Status: false,
+  information: ''
+})
+
+
+function saved() {
+  emit('save', UserData.value)
+}
+</script>
+<style>
+.create {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.265);
+  display: flex;
+  flex-direction: column;
+
+}
+
+.create form {
+  width: 500px;
+  margin: 100px auto;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  background: white;
+  padding: 40px;
+  border-radius: 20px;
+}
+
+input {
+  border: 1px solid black;
+  padding-left: 10px;
+  border-radius: 5px;
+  outline: none;
+}
+</style>
--- src/components/form.vue
+++ src/components/form.vue
@@ -1,45 +1,116 @@
 <template>
-  <div class="pa-4">
-    <button class="text-h5" @click="$router.push('/')">home</button>
-  </div>
+  <div class="flex justify-space-between">
+    <div class="pa-4">
+      <button class="text-h5" @click="$router.push('/')">home</button>
+    </div>
 
+    <div>
+      <div class="pa-4">
+        <button class="text-h5 bg-blue px-4 py-1 rounded-md
+     " @click="create = true">Create User</button>
+      </div>
+    </div>
+  </div>
+  <!-- {{ users }} -->
   <div class="w-50 mx-auto">
     <h1 class="text-h5 text-center mb-4">User List</h1>
-    <table class="border w-full ">
-      <thead class="border-b">
+    <table class="border-1 w-full ">
+      <thead class="border-1">
         <tr>
-          <th class="border-l  text-center">id</th>
-          <th>name</th>
-          <th>fullName</th>
-          <th>Middle Name</th>
-          <th>status</th>
-          <th>Information</th>
-          <th>control</th>
+          <th class="border-r  text-center" v-for="item in UserInfo">{{ item }}</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td class="border-l text-center">1</td>
-          <td>John</td>
-          <td>John Smith</td>
-          <td>Michael</td>
-          <td>Active</td>
-          <td>Some info</td>
+        <tr class="border-1" v-for="(user, idx) in users">
+          <td class="border-r text-center" v-for="(item, idx) in user">{{ item }}</td>
           <th>
             <div class="flex gap-2 pa-1 justify-center ">
-              <button class="bg-yellow px-2">edit</button>
-              <button class="bg-red px-2">delete</button>
+              <button class="bg-yellow px-2" @click="Edit(user)">edit</button>
+              <button class="bg-red px-2" @click="deleteUser(user)">delete</button>
             </div>
           </th>
         </tr>
       </tbody>
     </table>
   </div>
+  <creatUser v-if="create" @save="saved" :user="user" />
+
+
 </template>
-<script setup  lang="ts"  > 
+<script setup lang="ts">
+import axios from 'axios';
+import { ref, onMounted } from 'vue';
+import creatUser from './creatUser.vue';
 
+const create = ref(false)
+const API = ref('http://localhost:3000/')
+const users = ref([])
 
+const UserInfo: String[] = ['id', 'UserName', 'FullName', 'Middle Name', 'Status', 'Information', 'control']
 
+interface CreateUser {
+  id?: number
+  UserName: string,
+  fullName: string,
+  MiddleName: string,
+  Status: boolean,
+  information: string
+}
+
+const UserData = ref<CreateUser>({
+  UserName: '',
+  fullName: '',
+  MiddleName: '',
+  Status: false,
+  information: ''
+})
+
+const user = ref<creatUser[]>([])
+
+async function saved(UserData: CreateUser) {
+  try {
+    const res = await axios.post(`${API.value}users`, UserData)
+    user.value = res.data
+    console.log(res);
+  } catch (error) {
+    console.log(error);
+  }
+  create.value = !create.value
+  console.log(UserData.fullName);
+}
+
+async function deleteUser(idx: any) {
+  try {
+    const res = await axios.delete(`${API.value}users/${idx.id}`)
+    console.log(res);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function Edit(edit: any) {
+  try {
+    const res = await axios.get(`${API.value}users/${edit.id}`)
+    user.value = res.data
+    console.log(res.data);
+    create.value = !create.value
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getUserlist() {
+  try {
+    const res = axios.get(`${API.value}users`)
+    users.value = (await res).data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+onMounted(() => {
+  getUserlist()
+})
 </script>
 <style>
 /* table,thead,th {
